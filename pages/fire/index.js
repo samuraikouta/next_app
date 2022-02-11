b1ae09a85eed4e6cfa65aa89a8164a38164bbd2b
samuraikouta <kouta0316@gmail.com>
2022-02-11 16:59:21 +0900

    add firebase auth

--- pages/fire/index.js
+++ pages/fire/index.js
@@ -1,10 +1,14 @@
 import {useState, useEffect} from 'react'
 import Layout from '../../components/layout'
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { app } from '../../components/fire';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {getFirestore, collection, getDocs} from "firebase/firestore";
+import {app} from "../../components/fire";
 
 const db = getFirestore(app);
-// const db = getFirestore()
+const auth = getAuth()
+const provider = new GoogleAuthProvider();
+
+auth.signOut() //☆ログアウトする
 
 export default function Home() {
   const mydata = []
@@ -12,26 +16,37 @@ export default function Home() {
   const [message, setMessage] = useState('wait...')
 
   useEffect(() => {
-    const citiesCol = collection(db, 'mydata');
-    // const citySnapshot = await getDocs(citiesCol);
-
-    getDocs(citiesCol).then((snapshot)=> {
-      snapshot.forEach((document)=> {
-        const doc = document.data()
-        mydata.push(
-          <tr key={document.id}>
-            <td><a href={'/fire/del?id=' + document.id}>
-              {document.id}</a></td>
-            <td>{doc.name}</td>
-            <td>{doc.mail}</td>
-            <td>{doc.age}</td>
-          </tr>
-        )
-      })
-      setData(mydata)
-      setMessage('Firebase data.')
+    signInWithPopup(auth, provider).then(result=> {
+      setMessage('logined: ' + result.user.displayName)
+    }).catch((error) => {
+      setMessage('not logined.')
     })
-  }, [])
+  },[])
+
+  useEffect(() => {
+    if (auth.currentUser != null) {
+      const citiesCol = collection(db, 'mydata');
+      getDocs(citiesCol).then((snapshot)=> {
+        snapshot.forEach((document)=> {
+          const doc = document.data()
+          mydata.push(
+            <tr key={document.id}>
+              <td><a href={'/fire/del?id=' + document.id}>
+                {document.id}</a></td>
+              <td>{doc.name}</td>
+              <td>{doc.mail}</td>
+              <td>{doc.age}</td>
+            </tr>
+          )
+        })
+        setData(mydata)
+      })
+    } else {
+      mydata.push(
+        <tr key="1"><th colSpan="4">can't get data.</th></tr>
+      )
+    }
+  }, [message])
 
   return (
     <div>
